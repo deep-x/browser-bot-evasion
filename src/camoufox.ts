f59f7f545b310d6117a683cbf41861c6e0398707
Daniel Kurz <danielkurz03@protonmail.com>
2024-12-21 21:34:05 +0000

    update

--- src/camoufox.ts
+++ src/camoufox.ts
@@ -11,27 +11,16 @@ import firefox from "selenium-webdriver/firefox";
 // });
 
 const options = new firefox.Options()
-  .setBinary("/Applications/Camoufox.app/Contents/MacOS/camoufox")
-  .addArguments("--marionette")
-  .addArguments("--remote-debugging-port=9222")
-  .setPreference("marionette.port", 2828)
-  .setPreference("marionette.enabled", true);
-// .setBinary("/Applications/Firefox.app/Contents/MacOS/firefox");
+  // .setBinary("/Applications/Camoufox.app/Contents/MacOS/camoufox");
+  .setBinary("/Applications/Firefox.app/Contents/MacOS/firefox");
 // .addArguments("--headless")
 // .setPreference("devtools.debugger.remote-enabled", true)
 // .setPreference("devtools.chrome.enabled", true);
 // .merge(caps);
 
-const geckoPath = "/opt/homebrew/bin/geckodriver"; // Update this with actual path
-const service = new firefox.ServiceBuilder(geckoPath)
-  .setStdio("inherit") // Show us the logs
-  .setPort(4444)
-  .enableVerboseLogging(); // More detailed logs
-
 const driver = await new Builder()
   .forBrowser("firefox")
   .setFirefoxOptions(options)
-  .setFirefoxService(service)
   .build();
 
 // === === === === === === === === === === === === === === === === === ===
@@ -43,45 +32,45 @@ let screenshot = await driver.takeScreenshot();
 let buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
 await Bun.write("screenshots/camoufox-selenium-browserscan.png", buffer);
 
-console.log("https://bot-detector.rebrowser.net/");
-await driver.get("https://bot-detector.rebrowser.net/");
-await Bun.sleep(5000);
-screenshot = await driver.takeScreenshot();
-buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
-await Bun.write("screenshots/camoufox-selenium-bot-detector.png", buffer);
+// console.log("https://bot-detector.rebrowser.net/");
+// await driver.get("https://bot-detector.rebrowser.net/");
+// await Bun.sleep(5000);
+// screenshot = await driver.takeScreenshot();
+// buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
+// await Bun.write("screenshots/camoufox-selenium-bot-detector.png", buffer);
 
-console.log("https://bot.sannysoft.com/");
-await driver.get("https://bot.sannysoft.com/");
-await Bun.sleep(5000);
-screenshot = await driver.takeScreenshot();
-buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
-await Bun.write("screenshots/camoufox-selenium-sannysoft.png", buffer);
+// console.log("https://bot.sannysoft.com/");
+// await driver.get("https://bot.sannysoft.com/");
+// await Bun.sleep(5000);
+// screenshot = await driver.takeScreenshot();
+// buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
+// await Bun.write("screenshots/camoufox-selenium-sannysoft.png", buffer);
 
-console.log("https://abrahamjuliot.github.io/creepjs/");
-await driver.get("https://abrahamjuliot.github.io/creepjs/");
-await Bun.sleep(5000);
-screenshot = await driver.takeScreenshot();
-buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
-await Bun.write("screenshots/camoufox-selenium-creepjs.png", buffer);
+// console.log("https://abrahamjuliot.github.io/creepjs/");
+// await driver.get("https://abrahamjuliot.github.io/creepjs/");
+// await Bun.sleep(5000);
+// screenshot = await driver.takeScreenshot();
+// buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
+// await Bun.write("screenshots/camoufox-selenium-creepjs.png", buffer);
 
-console.log("https://www.browserscan.net/webrtc");
-await driver.get("https://www.browserscan.net/webrtc");
-await Bun.sleep(5000);
-screenshot = await driver.takeScreenshot();
-buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
-await Bun.write("screenshots/camoufox-selenium-webrtc.png", buffer);
+// console.log("https://www.browserscan.net/webrtc");
+// await driver.get("https://www.browserscan.net/webrtc");
+// await Bun.sleep(5000);
+// screenshot = await driver.takeScreenshot();
+// buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
+// await Bun.write("screenshots/camoufox-selenium-webrtc.png", buffer);
 
-console.log("https://proxy.incolumitas.com/proxy_detect.html");
-await driver.get("https://proxy.incolumitas.com/proxy_detect.html");
-await Bun.sleep(5000);
-screenshot = await driver.takeScreenshot();
-buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
-await Bun.write("screenshots/camoufox-selenium-proxy.png", buffer);
+// console.log("https://proxy.incolumitas.com/proxy_detect.html");
+// await driver.get("https://proxy.incolumitas.com/proxy_detect.html");
+// await Bun.sleep(5000);
+// screenshot = await driver.takeScreenshot();
+// buffer = new Uint8Array(Buffer.from(screenshot, "base64"));
+// await Bun.write("screenshots/camoufox-selenium-proxy.png", buffer);
 
-console.log("http://httpbin.org/ip");
-await driver.get("http://httpbin.org/ip");
-const pageSource = await driver.findElement(By.css("body"));
-console.log("Current IP:", await pageSource.getText());
+// console.log("http://httpbin.org/ip");
+// await driver.get("http://httpbin.org/ip");
+// const pageSource = await driver.findElement(By.css("body"));
+// console.log("Current IP:", await pageSource.getText());
 
-// await suspendForever();
 await driver.close();
+// await suspendForever();

65326eb41fe1495faec5dc7c57da386728bc9539
Daniel Kurz <danielkurz03@protonmail.com>
2024-12-21 20:22:24 +0000

    update

--- src/camoufox.ts
+++ src/camoufox.ts
@@ -2,26 +2,36 @@ import { parseProxyCredentials, suspendForever } from "./lib";
 import { Builder, By, Capabilities } from "selenium-webdriver";
 import firefox from "selenium-webdriver/firefox";
 
-const proxy = parseProxyCredentials(process.env["SOCKS_PROXY"]!);
-
-const caps = new Capabilities().setProxy({
-  proxyType: "manual",
-  socksProxy: `${proxy.host}:${proxy.port}`,
-  socksUsername: proxy.user,
-  socksPassword: proxy.pass,
-});
+// const proxy = parseProxyCredentials(process.env["SOCKS_PROXY"]!);
+// const caps = new Capabilities().setProxy({
+//   proxyType: "manual",
+//   socksProxy: `${proxy.host}:${proxy.port}`,
+//   socksUsername: proxy.user,
+//   socksPassword: proxy.pass,
+// });
 
 const options = new firefox.Options()
   .setBinary("/Applications/Camoufox.app/Contents/MacOS/camoufox")
-  // .setBinary("/Applications/Firefox.app/Contents/MacOS/firefox")
-  .addArguments("--headless")
-  .setPreference("devtools.debugger.remote-enabled", true)
-  .setPreference("devtools.chrome.enabled", true);
+  .addArguments("--marionette")
+  .addArguments("--remote-debugging-port=9222")
+  .setPreference("marionette.port", 2828)
+  .setPreference("marionette.enabled", true);
+// .setBinary("/Applications/Firefox.app/Contents/MacOS/firefox");
+// .addArguments("--headless")
+// .setPreference("devtools.debugger.remote-enabled", true)
+// .setPreference("devtools.chrome.enabled", true);
 // .merge(caps);
+
+const geckoPath = "/opt/homebrew/bin/geckodriver"; // Update this with actual path
+const service = new firefox.ServiceBuilder(geckoPath)
+  .setStdio("inherit") // Show us the logs
+  .setPort(4444)
+  .enableVerboseLogging(); // More detailed logs
 
 const driver = await new Builder()
   .forBrowser("firefox")
   .setFirefoxOptions(options)
+  .setFirefoxService(service)
   .build();
 
 // === === === === === === === === === === === === === === === === === ===
